f350c47e3d5f83a7bd07046e3c00476f8f76cb9f
Jonathan <jonretchless@gmail.com>
2020-03-04 22:29:20 -0800

    hooking up to back end

--- src/login/login.js
+++ src/login/login.js
@@ -2,15 +2,33 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Login extends React.Component {
+
+    handleSubmit(e){
+        e.preventDefault();
+        const data = new FormData(e.target);
+
+        fetch('http://localhost:8000/api/users/auth', {
+            method: 'POST',
+            body: data,
+        })
+        .then(res => res.json())
+        .then((data) => {
+            console.log(data);
+        })
+    }
     render() {
         return(
             <div className="Login-Container">
-            <form action="/index.html">
+            <form action=""
+                onSubmit={this.handleSubmit} >
                  <h2>Login</h2>
                  <label htmlFor="email">Email</label>
-                 <input type="text" id="email"></input>
+                 <input type="text" id="email" name="email"></input>
+                 <label htmlFor="password">Password</label>
+                 <input type="password" id="password" name="email"></input>
+ 
                  <div>
-                     <Link to="/home">Submit</Link>
+                     <button type="submit">Submit</button>
                  </div>
              </form>
          </div>
