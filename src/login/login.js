9c02abc2b498e4814ad7dcc95ec0a03753930911
Jonathan <jonretchless@gmail.com>
2020-03-04 22:51:03 -0800

    temp fix

--- src/login/login.js
+++ src/login/login.js
@@ -3,24 +3,25 @@ import { Link } from 'react-router-dom';
 
 export default class Login extends React.Component {
 
-    handleSubmit(e){
-        e.preventDefault();
-        const data = new FormData(e.target);
+    // handleSubmit(e){
+    //     e.preventDefault();
+    //     const data = new FormData(e.target);
 
-        fetch('http://localhost:8000/api/users/auth', {
-            method: 'POST',
-            body: data,
-        })
-        .then(res => res.json())
-        .then((data) => {
-            console.log(data);
-        })
-    }
+    //     fetch('http://localhost:8000/api/users/auth', {
+    //         method: 'POST',
+    //         body: data,
+    //     })
+    //     .then(res => res.json())
+    //     .then((data) => {
+    //         console.log(data);
+    //     })
+    // }
     render() {
         return(
             <div className="Login-Container">
             <form action=""
-                onSubmit={this.handleSubmit} >
+                // onSubmit={this.handleSubmit} 
+                >
                  <h2>Login</h2>
                  <label htmlFor="email">Email</label>
                  <input type="text" id="email" name="email"></input>
@@ -28,7 +29,7 @@ export default class Login extends React.Component {
                  <input type="password" id="password" name="email"></input>
  
                  <div>
-                     <button type="submit">Submit</button>
+                     <Link to="/home">submit</Link>
                  </div>
              </form>
          </div>
