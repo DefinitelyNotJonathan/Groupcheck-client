3a1be9a692ad77eac81783132d398ca0f8cd3382
Jonathan <jonretchless@gmail.com>
2020-03-17 16:40:21 -0700

    signup working

--- src/signUp/signUp.js
+++ src/signUp/signUp.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import ApiContext from '../ApiContext'
 
 export default class SignUp extends React.Component {
+
+    static contextType = ApiContext;
+
+    constructor(props) {
+        super(props);
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
     
     handleSubmit(e){
         e.preventDefault();
@@ -8,22 +18,24 @@ export default class SignUp extends React.Component {
         console.log(e.target)
         
         const data = new FormData(e.target);
+        let sendData = {};
         for (var key of data.entries()) {
-			console.log(key[0] + ', ' + key[1])
+    			console.log(key[0] + ', ' + key[1]);
+          sendData[key[0]] = key[1];
         }
-
+        console.log('sendData:');
+        console.log(sendData);
         fetch('http://localhost:8000/api/users', {
             method: 'POST',
             headers: {"Content-Type": "application/json"},
-            body: data
+            body: JSON.stringify(sendData)
         })
         .then(res => res.json())
         .then((user) => {
-            console.log('then');
-            console.log(user);
+            console.log(user)
             if(user && user.hasOwnProperty("id")) {
                 console.log('user id' + user.id)
-                this.context.setUser(user)
+                this.context.setUser({id: user.id})
                 console.log('did call this.context.setUser()');
                 console.log('check user state:');
                 console.log(this.context.user);
@@ -38,14 +50,14 @@ export default class SignUp extends React.Component {
             <div>
                 <form onSubmit={this.handleSubmit}>
                   <h2>Sign Up</h2>
-                  <label htmlFor="First name">First Name*</label>
-                  <input type="text" id="First name" name="First name"></input>
-                  <label htmlFor="Last name">Last Name*</label>
-                  <input type="text" id="Last name" name="Last name"></input>
-                  <label htmlFor="Email">Email*</label>
-                  <input type="text" id="Email" name="First Email"></input>
-                  <label htmlFor="Password" id="Password" name="Password">Password*</label>
-                  <input type="text" id="Password" name="Password"></input>
+                  <label htmlFor="firstname">First Name*</label>
+                  <input type="text" id="firstname" name="firstname"></input>
+                  <label htmlFor="lastname">Last Name*</label>
+                  <input type="text" id="lastname" name="lastname"></input>
+                  <label htmlFor="email">Email*</label>
+                  <input type="text" id="email" name="email"></input>
+                  <label htmlFor="p_word" id="p_word" name="p_word">Password*</label>
+                  <input type="text" id="p_word" name="p_word"></input>
                   <button id= "signup" name="signup" >Sign Up!</button>
                 </form>
             </div>
